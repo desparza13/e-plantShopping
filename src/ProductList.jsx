eb4d6f6029194882855e8bff1775ed7d6be2b192
desparza13 <desparzaespinosa@gmail.com>
2025-07-12 23:34:11 -0400

    Fix typo

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -294,7 +294,7 @@ function ProductList({ onHomeClick }) {
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
                     <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
-                
+                    {calculateTotalQuantity()}
                 </div>
             </div>
             {!showCart ? (
@@ -304,8 +304,8 @@ function ProductList({ onHomeClick }) {
               <h2 className="plant_heading">{section.category}</h2>
               <div className="product-list">
                 {section.plants.map((plant, plantIndex) => {
-                  const isInCart = cart.items.some(item => item.name === plant.name);
-                  return (
+                const isInCart = CartItems.some(item => item.name === plant.name);
+                return (
                     <div className="product-card" key={plantIndex}>
                       <h3 className="product-title">{plant.name}</h3>
                       <img className="product-image" src={plant.image} alt={plant.name} />
